62d0b2002b89575dd2891391802cab64845b0aa3
Gavin Lochtefeld <gavin.loch@gmail.com>
2020-08-30 17:31:57 -0500

    Basic movement
    Created project; character can move/jump/crouch.

---
+++ Assets/Scripts/Characters/Player/Movement2D.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Movement2D : MonoBehaviour
+{
+    public Controller2D controller;
+    public float runSpeed = 40f;
+
+    private float _horizontalMove = 0f;
+    private bool _jumping;
+    private bool _crouching;
+    
+    private void FixedUpdate()
+    {
+        controller.Move(_horizontalMove * Time.fixedDeltaTime, _crouching, _jumping);
+        _jumping = false;
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        var input = context.ReadValue<Vector2>();
+        _horizontalMove = input.x * runSpeed;
+        if (input.y > 0)
+            _jumping = true;
+        if (input.y < 0)
+            _crouching = true;
+        else
+            _crouching = false;
+    }
+}
+
+/* This uses the new InputSystem package for unity, which is more 
+   efficient because it relies on events being fired off instead of
+   the Update() method. I think. */
